2cb80fb31b46440c8cfb7df3f72422d3521b3654
mehedi-lowp <mehedi@lowp.jp>
2024-07-04 00:44:16 +0600

    added trailing /

--- routes/test/test.js
+++ routes/test/test.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
 
-router.get("", (req, res)=>{
+router.get("/", (req, res)=>{
     // #swagger.tags = ['Tests']
     res.send("Test route get");
 });
 
-router.post("", (req, res)=>{
+router.post("/", (req, res)=>{
     // #swagger.tags = ['Tests']
     res.send("Test route post");
 });
