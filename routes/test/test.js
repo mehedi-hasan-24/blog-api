9d953d929d2d1c2dfe007a42e0464bfbcf0502e5
mehedi-lowp <mehedi@lowp.jp>
2024-07-04 00:42:08 +0600

    added Tests tag to post route

--- routes/test/test.js
+++ routes/test/test.js
@@ -1,20 +1,13 @@
 const router = require("express").Router();
 
-/**
- * @swagger
- * /tests:
- *   get:
- *     description: Get all users
- *     responses:
- *       200:
- *         description: Success
- */
-router.get("/", (req, res)=>{
+
+router.get("", (req, res)=>{
     // #swagger.tags = ['Tests']
     res.send("Test route get");
 });
 
-router.post("/", (req, res)=>{
+router.post("", (req, res)=>{
+    // #swagger.tags = ['Tests']
     res.send("Test route post");
 });
 
