d8c50705ca7428d81ebb31af9353ee06deb810d9
mehedi-lowp <mehedi@lowp.jp>
2024-07-12 10:47:18 +0600

    added docker-compose and connected mongodb

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const mongoose = require("mongoose");
 const app = express();
-const port = 8000
+const port = 8000;
 const allRoutes = require("./routes/index.js");
 const appRouter = express.Router();
 
@@ -12,8 +14,16 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.get('/', (req, res) => {
 res.send('Hello World!')
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+const DB_NAME = process.env.DB_NAME;
+
+const getDbUrlString = ()=>{
+    let DB_CONNECTION_URL = process.env.DB_CONNECTION_URL;
+    DB_CONNECTION_URL = DB_CONNECTION_URL.replace('<username>', process.env.DB_USERNAME);
+    DB_CONNECTION_URL = DB_CONNECTION_URL.replace('<password>', process.env.DB_PASSWORD);
+    return DB_CONNECTION_URL;
+}
+mongoose.connect(getDbUrlString(), { dbName: process.env.DB_NAME }).then(    
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
     })
-
-
+);
